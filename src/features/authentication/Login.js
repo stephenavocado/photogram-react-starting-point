e52c4150a24742d78ddd12cef6ad6df3fcfa34fc
stephenavocado <stephenwalter@chicagobooth.edu>
2022-05-17 15:11:18 +0000

    starting-point-feed

--- src/features/authentication/Login.js
+++ src/features/authentication/Login.js
@@ -1,8 +1,6 @@
 import React, { useContext, useState } from "react";
-import { AuthenticationContext } from "../../services/authentication/authentication.context";
 
 export default function Login() {
-    const { currentUser, credentials, handleInput, loginRequest } = useContext(AuthenticationContext);
     
     return (
         <div className="page-wrapper">
@@ -12,19 +10,14 @@ export default function Login() {
                     className="form-input"   
                     type="text" 
                     name="email"
-                    value={credentials.email} 
-                    onChange={handleInput}
                 />
                 <input 
                     className="form-input"   
                     type="password" 
                     name="password"
-                    value={credentials.password} 
-                    onChange={handleInput}
                 />
                 <button 
                     className="form-button"
-                    onClick={loginRequest}
                 >
                     Login
                 </button>
--- src/features/feed/Feed.js
+++ src/features/feed/Feed.js
@@ -1,20 +1,12 @@
 import React, { useState } from "react";
-import Post from "./Post";
 import data from "../feed/data/data";
 
 export default function Feed() {
-    const [feed, setFeed] = useState(data);
-
-    const posts = feed.map((post) => {
-        return (
-            <Post post={post} key={post.id} />
-        )
-    });
+    const [feed, setFeed] = useState();
   
     return (
         <div className="page-wrapper">
             <h1>feed</h1>
-            {posts}
         </div>
     );
 };
